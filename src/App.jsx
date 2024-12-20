d29d1dc1b38b82ca7319dada1d6854a31c5881c2
gnanesmk <gnaneshwer.mk@sscinc.com>
2024-12-20 18:30:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import UserRating from './components/UserRating'
+
+function App() {
+  return (
+      <>
+      <h2>Lists of all projects here</h2>
+      <div>
+        <div>
+          <p>1. User Rating</p>
+          <UserRating/>
+        </div>
+      </div>
+      </>
+  )
+}
+
+export default App
---
+++ src/components/UserRating.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+
+const UserRating = () => {
+  const [rating, setRating] = useState(0)
+  const [hoveredIndices, setHoveredIndices] = useState(0)
+  const changeRating = (rating)=>{
+    setRating(rating)
+  }
+  const addIndex = (index)=>{
+    setHoveredIndices(index)
+  }
+  const removeIndex = ()=>{
+    setHoveredIndices(0)
+  }
+
+
+  const buttons = ['button1','button2','button3','button4','button5']
+  
+  return (
+    <div className="card" onMouseLeave={removeIndex}>
+        {
+          buttons.map((button,i)=>(
+            <button key={i} onMouseEnter={()=>addIndex(i+1)}
+              className={i+1<=hoveredIndices|| i+1<=rating ?'hovered':''}
+              onClick={()=>changeRating(i+1)}>
+               X
+            </button>
+          ))
+        }
+        
+        <p>
+          Your Rating is {rating}
+        </p>
+      </div>
+  )
+}
+
+export default UserRating
